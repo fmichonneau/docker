fc42450e1f2a9cd9cd8ceb8299fdc0c2c54af692
Vincent Nadoll <vincent.nadoll@gmail.com>
2019-04-04 21:35:56 +0200

    Fix backup file name

--- backup-mysql/backup.sh
+++ backup-mysql/backup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 date="$(date +%Y-%m-%d_%H:%M)"
-file="/backups/${date}.dump.tar.gz"
+file="/backups/${date}.dump.sql.gz"
 
 echo "[Backup] ${date} *** Dumping MySQL database..."
 mysqldump -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} | gzip -9 > "${file}"
 echo "[Backup] ${date} *** Done"
 
-if [ "$(ls -l /backups/*.dump.tar.gz | grep -v ^l | wc -l)" -gt "${BACKUP_ROTATIONS}" ]; then
+if [ "$(ls -l /backups/*.dump.sql.gz | grep -v ^l | wc -l)" -gt "${BACKUP_ROTATIONS}" ]; then
   echo "[Backup] ${date} *** Removing deprecated dumps..."
-  ls -F /backups/*.dump.tar.gz | head -n -${BACKUP_ROTATIONS} | xargs rm
+  ls -F /backups/*.dump.sql.gz | head -n -${BACKUP_ROTATIONS} | xargs rm
   echo "[Backup] ${date} *** Done"
 fi
 
